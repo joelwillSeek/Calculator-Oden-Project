6ada774fe7dd45f3cbc9179402bce36a79e9eac5
joel <joelthousend@gmail.com>
2023-06-11 12:51:59 +0300

    Started

---
+++ script.js
@@ -0,0 +1,13 @@
+/**TODO
+ * ->Add function for add, minus, multiply and division
+ * ->function named operate that has parameter of firstNum,lastNum,operator that picks the operations functions
+ * ->create a function to update display
+ * ->store number in display when operator is click and the operator clicked
+ * ->call operate on = pressed
+ * ->Once the operate is called then display solution
+ * **/
+
+let firstNumber;
+let operator;
+let lastNumber;
+let display;
